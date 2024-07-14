302a98bbcb425be14eb8ebe6cd7e251b906610a8
naieli <loebleinnaieli@gmail.com>
2024-07-14 20:06:39 -0300

    planner-app

---
+++ src/app/trip/details.tsx
@@ -0,0 +1,150 @@
+import { useEffect, useState } from "react";
+import { Button } from "@/components/button";
+import { Modal } from "@/components/modal";
+import { colors } from "@/styles/colors";
+import { Plus } from "lucide-react-native";
+import { Alert, Text, View, FlatList } from "react-native";
+import { Input } from "@/components/input";
+import { validateInput } from "@/utils/validateInput";
+import { linksServer } from "@/server/links-server";
+import { TripLink, TripLinkProps } from "@/components/tripLink";
+import { participantsServer } from "@/server/participants-server";
+import { Participant, ParticipantProps } from "@/components/participant";
+
+export function Details({ tripId }: { tripId: string }) {
+  // MODAL
+  const [showNewLinkModal, setShowNewLinkModal] = useState(false);
+
+  // LOADING
+  const [isCreatingLinkTrip, setIsCreatingLinkTrip] = useState(false);
+
+  // DATA
+  const [linkTitle, setLinkTitle] = useState("");
+  const [linkURL, setLinkURL] = useState("");
+
+  // LISTS
+  const [links, setLinks] = useState<TripLinkProps[]>([]);
+  const [participants, setParticipants] = useState<ParticipantProps[]>([]);
+
+  function resetNewLinkFields() {
+    setLinkTitle("");
+    setLinkURL("");
+    setShowNewLinkModal(false);
+  }
+
+  async function handleCreateTripLink() {
+    try {
+      if (!linkTitle.trim()) {
+        Alert.alert("Link", "Informe um título para o link!");
+      }
+
+      if (!validateInput.url(linkURL.trim())) {
+        Alert.alert("Link", "Link inválido!");
+      }
+
+      setIsCreatingLinkTrip(true);
+
+      await linksServer.create({
+        tripId,
+        title: linkTitle,
+        url: linkURL,
+      });
+
+      Alert.alert("Link", "Link criado com sucesso!");
+      resetNewLinkFields();
+      await getTripLinks();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsCreatingLinkTrip(false);
+    }
+  }
+
+  async function getTripLinks() {
+    try {
+      const links = await linksServer.getLinksByTripId(tripId);
+      setLinks(links);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function getTripParticipants() {
+    try {
+      const participants = await participantsServer.getByTripId(tripId);
+      setParticipants(participants);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getTripLinks();
+    getTripParticipants();
+  }, []);
+
+  return (
+    <View className="flex-1 mt-10">
+      <Text className="text-zinc-50 text-2xl font-semibold mb-2">
+        Links importantes
+      </Text>
+
+      <View className="flex-1">
+        {links.length > 0 ? (
+          <FlatList
+            data={links}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <TripLink data={item} />}
+            contentContainerClassName="gap-4"
+          />
+        ) : (
+          <Text className="text-zinc-400 font-regular text-base mt-2 mb-6">
+            Nenhum link adicionado.
+          </Text>
+        )}
+
+        <Button variant="secondary" onPress={() => setShowNewLinkModal(true)}>
+          <Plus color={colors.zinc[200]} size={20} />
+          <Button.Title>Adicionar link</Button.Title>
+        </Button>
+      </View>
+
+      <View className="flex-1 border-t border-zinc-800 mt-6">
+        <Text className="text-zinc-50 text-2xl font-semibold my-6">
+          Convidados
+        </Text>
+
+        <FlatList
+          data={participants}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Participant data={item} />}
+          contentContainerClassName="gap-4 pb-44"
+        />
+      </View>
+
+      <Modal
+        title="Cadastrar link"
+        subtitle="Todos os convidados podem visualizar os links."
+        visible={showNewLinkModal}
+        onClose={() => setShowNewLinkModal(false)}
+      >
+        <View className="gap-2 mb-3">
+          <Input variant="secondary">
+            <Input.Field
+              placeholder="Título do link"
+              onChangeText={setLinkTitle}
+            />
+          </Input>
+
+          <Input variant="secondary">
+            <Input.Field placeholder="URL" onChangeText={setLinkURL} />
+          </Input>
+        </View>
+
+        <Button isLoading={isCreatingLinkTrip} onPress={handleCreateTripLink}>
+          <Button.Title>Salvar link</Button.Title>
+        </Button>
+      </Modal>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// IMPORTAÇÃO DOS TOKENS PARA CUSTOMIZAR TEMA
+
+import { colors } from "./src/styles/colors";
+import { fontFamily } from "./src/styles/fontFamily";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors,
+      fontFamily,
+    },
+  },
+  plugins: [],
+};
